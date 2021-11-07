1fa318b1a20134fa4b8cc96b28c877da2dfe7f48
Jose Rincon <rincon.jmg@gmail.com>
2021-11-07 08:28:20 -0500

    ajustando el workflow de gulp y creando el header"

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require( 'sass' ));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 
-    src('./src/scss/app.scss') //Identificar el archivo .scss a compilar
+    src('./src/scss/**/*.scss') //Identificar el archivo .scss a compilar
+        .pipe( plumber() ) //Evita que se detenga la ejecución del gulp (workflow) al toparse con un error
         .pipe( sass() ) //Compilar el archivo .scss a .css
         .pipe( dest('./build/css') ) // Almacenarlo en el disco duro
 
@@ -11,7 +13,7 @@ function css( done ) {
 }
 
 function dev( done ) {
-    watch( './src/scss/app.scss', css );
+    watch( './src/scss/**/*.scss', css );
     done();
 }
 
