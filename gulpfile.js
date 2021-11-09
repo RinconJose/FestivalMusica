64853eb35a8c41a2468d10991c7eea2d449a3abb
Jose Rincon <rincon.jmg@gmail.com>
2021-11-09 17:42:58 -0500

    finalizando la seccion de la galeria con la funcionalidad de overlay

--- gulpfile.js
+++ gulpfile.js
@@ -20,15 +20,22 @@ function css( done ) {
     done();
 }
 
+function javascript( done ) {
+    
+    src( './src/js/**/*.js' )
+        .pipe( dest('./build/js') )
+    done();
+}
+
 function imagenes( done ) {
 
     const opciones = {
         optimizationLevel: 3
     }
 
-    src('src/img/**/*{png,jpg}')
+    src('./src/img/**/*{png,jpg}')
         .pipe( cache( imagemin(opciones) ) )
-        .pipe( dest('build/img') )
+        .pipe( dest('./build/img') )
     done();
 }
 
@@ -38,9 +45,9 @@ function versionWebp( done ) {
         quality: 50
     };
 
-    src('src/img/**/*{png,jpg}')
+    src('./src/img/**/*{png,jpg}')
         .pipe( webp(opciones) )
-        .pipe( dest('build/img') )
+        .pipe( dest('./build/img') )
     done();
 }
 
@@ -50,19 +57,21 @@ function versionAvif( done ) {
         quality: 50
     };
 
-    src('src/img/**/*{png,jpg}')
+    src('./src/img/**/*{png,jpg}')
         .pipe( avif(opciones) )
-        .pipe( dest('build/img') )
+        .pipe( dest('./build/img') )
     done();
 }
 
 function dev( done ) {
     watch( './src/scss/**/*.scss', css );
+    watch( './src/js/**/*.js', javascript );
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel( imagenes, versionWebp, versionAvif, dev );
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev );
