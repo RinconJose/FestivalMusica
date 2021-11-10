8a51ce9d1d54e4e77fbe4c2564bfefceafb2508a
Jose Rincon <rincon.jmg@gmail.com>
2021-11-10 07:15:46 -0500

    ajustando el gulp para comprimir archivos de js y css - finalizando proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require( 'sass' ));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer'); // Va segurarse de funcionar en el navegador que le digas
+const cssnano = require('cssnano'); // lo comprime de una mejor manera
+const postcss = require('gulp-postcss'); // le hace algunas transformaciones por medio de los dos anterioes
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -10,19 +14,27 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser-js');
+
 function css( done ) {
 
     src('./src/scss/**/*.scss') //Identificar el archivo .scss a compilar
+        .pipe( sourcemaps.init() )
         .pipe( plumber() ) //Evita que se detenga la ejecución del gulp (workflow) al toparse con un error
         .pipe( sass() ) //Compilar el archivo .scss a .css
+        .pipe( postcss([autoprefixer(), cssnano()]))
+        .pipe( sourcemaps.write('.') )
         .pipe( dest('./build/css') ) // Almacenarlo en el disco duro
-
     done();
 }
 
 function javascript( done ) {
     
     src( './src/js/**/*.js' )
+        .pipe( sourcemaps.init() )
+        .pipe( terser() )
+        .pipe( sourcemaps.write('.') )
         .pipe( dest('./build/js') )
     done();
 }
